f12d6b67bfc9710bc75b5e5cbf6bc77336daaf6c
Md. Shoriful Islam Tarek <sitarek77@gmail.com>
2024-08-05 22:05:01 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {},
+    
+  },
+  plugins: [],
+  
+  // npx tailwindcss -i ./input.css -o ./output.css --watch
+}
+
